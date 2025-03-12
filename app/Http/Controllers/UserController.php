8fc9d2f76cce2e2ae3a69e2e281d0e73f7963002
kibarmustofa <kibarmustofa115@gmail.com>
2025-03-12 07:46:43 +0700

    pertemuan 4 prak 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
         //UserModel::create($data);
         
         // coba akses model UserModel
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        $jumlahPengguna = UserModel::where('level_id', 2)->count();
+        return view('user', ['jumlahPengguna' => $jumlahPengguna]);
     }
 }
