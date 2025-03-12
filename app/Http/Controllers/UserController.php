80da199e6f0b76e1e3ad47068cfa3289fe894d58
kibarmustofa <kibarmustofa115@gmail.com>
2025-03-12 07:16:42 +0700

    pertemuan 4 praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,26 @@ class UserController extends Controller
 {
     public function index()
     {
+        //pertemuan 3
         // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        //$data = [
+        //    'nama' => 'Pelanggan Pertama',
+        //];
+        //UserModel::where('username', 'customer-1')->update($data); // update data user
 
+        //$data = [
+        //    'level_id' => 2,
+        //    'username' => 'manager_tiga',
+        //    'nama' => 'Manager 3',
+        //    'password' => Hash::make('12345')
+        //];
+        //UserModel::create($data);
+        
         // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::findor(20
+        , ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
